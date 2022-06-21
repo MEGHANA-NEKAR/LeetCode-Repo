d9f84fe8ee5e133052be0c440e55908e078cfe7c
MEGHANA NEKAR <101660984+MEGHANA-NEKAR@users.noreply.github.com>
2022-06-21 22:16:31 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (7.28%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        vector <int> nums(n);
+      
+        for(int i=0;i<n;i++){
+            nums[i]=start+(2*i);
+            
+        }
+        int ans = nums[0];
+        for(int j=1;j<n;j++){
+            ans = ans ^ nums[j];
+        }
+        return ans;
+    }
+};
+//time complexity:O(n)
+//space complexity:O(n)
