3220872c30a2bb25b03253f9d7d7c32c71bac293
MEGHANA NEKAR <101660984+MEGHANA-NEKAR@users.noreply.github.com>
2022-06-21 08:27:20 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (68.28%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int count =0;
+        return count+(n-1);
+        
+    }
+};
+//time complexity: O(1)
+//space complexity:O(1)
