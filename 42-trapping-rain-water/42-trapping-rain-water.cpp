5633e562ef22789a1bf95d3f5db8481cb86e9742
MEGHANA NEKAR <101660984+MEGHANA-NEKAR@users.noreply.github.com>
2022-06-22 14:17:42 +0530

    Time: 20 ms (61.92%), Space: 20.6 MB (15.76%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n= height.size();
+        if(n==1) return 0;
+        
+        vector<int> l(n,height[0]);
+        vector<int> r(n,height[n-1]);
+        for(int i=1;i<n;i++){
+            l[i]=max(height[i],l[i-1]);
+        }
+        for(int i=n-2;i>=0;i--){
+            r[i] =max(height[i],r[i+1]);
+        }
+        int w=0;
+        for(int i=0;i<n;i++){
+            int h= min(l[i],r[i]);
+            int neth= h-height[i];
+            w += neth;
+        }
+        return w;
+    }
+};
+//time complexity:O(n)
+//space complexity:O(n)
