7db9bc2b28eec35ccd03b7fe6581e5f3c29939ea
MEGHANA NEKAR <101660984+MEGHANA-NEKAR@users.noreply.github.com>
2022-07-01 23:18:09 +0530

    Time: 12 ms (11.13%), Space: 7.1 MB (38.63%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    unordered_map<string, string> n;
+    int num=0;
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        num++;
+        string ans="http://tinyurl.com/";
+        string add=to_string(num);
+        ans+=add;
+        n[ans]=longUrl;
+        return ans;
+        
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return n[shortUrl];
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
+
+//time complexity:O(n)
+//space complexity:O(n)
