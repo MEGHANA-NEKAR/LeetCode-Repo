d0a551cb96b49a1966669397740ccf6263b4fb4d
MEGHANA NEKAR <101660984+MEGHANA-NEKAR@users.noreply.github.com>
2023-08-15 10:31:53 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -6,28 +6,28 @@ using namespace std;
 // } Driver Code Ends
 
 class Solution
-{   private:
-        int f(int ind,int W,int wt[], int val[],vector<vector<int>>&dp){
-            int mod = 1e9+7;
-            if(ind == 0){
-                if(wt[0] <= W) return val[0];
-                else return 0;
-            }
-            if(dp[ind][W] != -1) return dp[ind][W];
-            int notTake = f(ind-1,W,wt,val,dp);
-            int take = INT_MIN;
-            if (W >= wt[ind]) {
-                take = val[ind] + f(ind-1, W - wt[ind], wt, val, dp);
-            }
-            
-            return dp[ind][W] = max(take,notTake)%mod;
-        }
+{   
     public:
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-        vector<vector<int>>dp(n,vector<int>(W+1,-1));
-        return f(n-1,W,wt,val,dp);
+        vector<vector<int>>dp(n,vector<int>(W+1,0));
+        for(int i=wt[0];i<=W;i++){
+            dp[0][i] = val[0];
+        }
+        
+        for(int ind=1;ind<n;ind++){
+            for(int w=0;w<=W;w++){
+                int notTake = dp[ind-1][w];
+                int take = INT_MIN;
+                if(wt[ind] <= w)
+                {
+                    take = val[ind] + dp[ind-1][w-wt[ind]];
+                };
+                dp[ind][w] = max(notTake,take);
+            }
+        }
+        return dp[n-1][W];
     }
 };
 
