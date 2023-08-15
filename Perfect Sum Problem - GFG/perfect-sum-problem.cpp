46fd465c313fd0adc19a15ace3ab5a3a4bdc1b2a
MEGHANA NEKAR <101660984+MEGHANA-NEKAR@users.noreply.github.com>
2023-08-15 09:47:29 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -5,30 +5,25 @@ using namespace std;
 // } Driver Code Ends
 
 class Solution{
-    private:
-    int f(int ind,int sum,int arr[],vector<vector<int>>&dp){
-        int mod = 1e9+7;
-        if(ind == 0) {
-            if(sum == 0 && arr[0] == 0) return 2;
-            if(sum == 0 || sum == arr[0]) return 1;
-            return 0;
-        }
-        if(dp[ind][sum] != -1) return dp[ind][sum];
-        
-        int notTake = f(ind-1,sum,arr,dp)%mod;
-        int take= 0;
-        if(sum>=arr[ind]){
-            take = f(ind-1,sum-arr[ind],arr,dp)%mod;
-        }
-        
-        return dp[ind][sum] = (take+notTake)%mod;
-    }
-    
+   
 	public:
 	int perfectSum(int arr[], int n, int sum)
 	{
-	    vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-        return f(n-1,sum,arr,dp);
+	    vector<vector<int>>dp(n,vector<int>(sum+1,0));
+	    int mod = 1e9+7;
+	    if(arr[0] == 0) dp[0][0] = 2;
+	    else dp[0][0] = 1;
+	    if(arr[0] != 0 && arr[0] <= sum) dp[0][arr[0]] = 1;
+	    
+	    for(int ind = 1;ind<n;ind++){
+	        for(int target = 0;target <=sum;target++){
+	            int notTake = dp[ind-1][target];
+	            int take = 0;
+	            if(target>=arr[ind]) take = dp[ind-1][target - arr[ind]];
+	            dp[ind][target] = (notTake + take)%mod;
+	        }
+	    }
+        return dp[n-1][sum];
 	}
 	  
 };
