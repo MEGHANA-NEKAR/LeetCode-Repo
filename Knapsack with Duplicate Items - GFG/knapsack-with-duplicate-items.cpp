f982d4d01f4bd0b4e75ae9bc9d36414b2e7a3668
MEGHANA NEKAR <101660984+MEGHANA-NEKAR@users.noreply.github.com>
2023-08-15 13:59:42 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -8,22 +8,23 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-private:
-    int f(int ind,int target,int val[], int wt[],vector<vector<int>>&dp ){
-        if(ind == 0){
-                return ((int)(target/wt[0]))*val[0];
-        }
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        int notTake = f(ind-1,target,val,wt,dp);
-        int take = 0;
-        if(target >= wt[ind]) take = val[ind] + f(ind,target - wt[ind],val,wt,dp);
-        return dp[ind][target] = max(take,notTake);
-    }
+
 public:
     int knapSack(int N, int W, int val[], int wt[])
     {   
-        vector<vector<int>>dp(N,vector<int>(W+1,-1));
-        return f(N-1,W,val,wt,dp);
+        vector<vector<int>>dp(N,vector<int>(W+1,0));
+        for(int t=0;t<=W;t++){
+            dp[0][t] = ((int)(t/wt[0]))*val[0];
+        }
+        for(int ind=1;ind<N;ind++){
+            for(int target = 0;target<=W;target++){
+                int notTake = dp[ind-1][target];
+                int take = 0;
+                if(target >= wt[ind]) take = val[ind] + dp[ind][target - wt[ind]];
+                dp[ind][target] = max(take,notTake);
+            }
+        }
+        return dp[N-1][W];
     }
 };
 
