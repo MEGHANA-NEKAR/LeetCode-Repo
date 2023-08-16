3f471be26dd81240e6657586c8c7d3d3385b82f3
MEGHANA NEKAR <101660984+MEGHANA-NEKAR@users.noreply.github.com>
2023-08-16 12:11:31 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.cpp
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -9,18 +9,19 @@ class Solution{
     
     int longestCommonSubstr (string S1, string S2, int n, int m)
     {   
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        vector<int>prev(m+1,0),curr(m+1,0);
         int ans = 0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(S1[i-1] == S2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    ans = max(ans,dp[i][j]);
+                    curr[j] = 1 + prev[j-1];
+                    ans = max(ans,curr[j]);
                 }
                 else{
-                    dp[i][j] = 0;
+                    curr[j] = 0;
                 }
             }
+            prev = curr;
         }
         return ans;
     }
