2c2189488604e02d54abe5e844d30f788bef050d
MEGHANA NEKAR <101660984+MEGHANA-NEKAR@users.noreply.github.com>
2023-08-27 19:50:15 +0530

    Added solution - LeetHub

--- Chocolates Pickup - GFG/chocolates-pickup.cpp
+++ Chocolates Pickup - GFG/chocolates-pickup.cpp
@@ -6,35 +6,36 @@ using namespace std;
 // } Driver Code Ends
 
 class Solution {
-  private:
-    int f(int i,int j1,int j2,vector<vector<int>>& grid,vector<vector<vector<int>>>&dp){
-        int n = grid.size();
-        int m = grid[0].size();
-        if(j1<0 || j1>=m || j2<0 || j2>=m) return -1e9;
-        if(i == n-1){
-            if(j1 == j2) return grid[i][j1];
-            else return grid[i][j1] + grid[i][j2];
-        }
-        if(dp[i][j1][j2]!=-1)return dp[i][j1][j2];
-        int maxi = 0;
-        for(int dj1 = -1;dj1<=1;dj1++){
-            for(int dj2=-1;dj2<=1;dj2++){
-                int total = 0;
-                if(j1 == j2){
-                    total = grid[i][j1];
-                }else{
-                    total = grid[i][j1] + grid[i][j2];
-                }
-                total += f(i+1,j1+dj1 ,j2+dj2,grid,dp);
-                maxi = max(maxi,total);
-            }
-        }
-        return dp[i][j1][j2] = maxi;
-    }
+ 
   public:
     int solve(int n, int m, vector<vector<int>>& grid) {
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return f(0,0,m-1,grid,dp);
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,0)));
+        for(int j1=0;j1<m;j1++){
+            for(int j2=0;j2<m;j2++){
+                if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j1=0;j1<m;j1++){
+                for(int j2=0;j2<m;j2++){
+                    int maxi = 0;
+                    for(int dj1 = -1;dj1<=1;dj1++){
+                        for(int dj2=-1;dj2<=1;dj2++){
+                            int total = 0;
+                            if(j1 == j2) total = grid[i][j1];
+                            else total = grid[i][j1] + grid[i][j2];
+                            if(j1+dj1 >= 0 && j1+dj1 < m && j2+dj2>=0 && j2+dj2 < m){
+                                total += dp[i+1][j1+dj1][j2+dj2];
+                                maxi = max(maxi,total);
+                            }
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
     }
 };
 
